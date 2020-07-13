278935ee2b980fa2d0c3471f05bc4389af9ed770
DaniloLima122 <limadanilo06@gmail.com>
2020-07-13 16:51:17 -0300

    Criação da estrutura HTML do site

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task("sass", () => {
+
+    return gulp.src("./sass/*.scss")
+      .pipe(sass())
+      .pipe(gulp.dest("./css"))
+});
+
+gulp.task("watch", () => {
+    gulp.watch("./sass/*.scss", gulp.series(["sass"]));
+})
+
+gulp.task("default" , gulp.series(["sass","watch"]));
